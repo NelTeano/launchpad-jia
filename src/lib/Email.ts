e04585e83bb31efe05db10a8abcb021fb4f8615f
NelTeano <jonelteano29@gmail.com>
2025-11-07 21:54:38 +0800

    initial install packages/ comment out email sender

--- src/lib/Email.ts
+++ src/lib/Email.ts
@@ -6,21 +6,21 @@ export async function sendEmail({
   html,
   subject = "JIA Update: Your Application Has Been Successfully Updated",
 }) {
-  const mailgun = new Mailgun(formData);
-  const mg = mailgun.client({
-    username: "api",
-    key: process.env.MAILGUN_API_KEY,
-  });
+  // const mailgun = new Mailgun(formData);
+  // const mg = mailgun.client({
+  //   username: "api",
+  //   key: process.env.MAILGUN_API_KEY,
+  // });
 
   try {
-    const msg = await mg.messages.create("hellojia.ai", {
-      from: "noreply@hellojia.ai",
-      to: [recipient],
-      subject: subject,
-      html: html,
-    });
+    // const msg = await mg.messages.create("hellojia.ai", {
+    //   from: "noreply@hellojia.ai",
+    //   to: [recipient],
+    //   subject: subject,
+    //   html: html,
+    // });
 
-    console.log(msg);
+    // console.log(msg);
     return {
       success: true,
       message: "Email sent successfully",
